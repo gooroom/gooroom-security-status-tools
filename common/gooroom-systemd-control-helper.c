a5a396a1b4bd127fb736659642861a3233953d31
user <cjheo@gooroom.kr>
2019-11-29 13:41:28 +0900

    Update sources regarding copyright
    Change-Id: I393dca1976c0ddcef313baaa9e05a84ffa68d003

--- common/gooroom-systemd-control-helper.c
+++ common/gooroom-systemd-control-helper.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2013 Intel, Inc
- * Copyright (C) 2015-2019 Gooroom <gooroom@gooroom.kr>
+ * Copyright (C) 2018-2019 Gooroom <gooroom@gooroom.kr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
